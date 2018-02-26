8a66ac58451c3fb24312d9b45f77fb49bb25a37f
Michael Ashley <nexues97@users.noreply.github.com>
2018-02-26 17:35:58 -0500

    Updated JS for designer page

--- javascript/designer.js
+++ javascript/designer.js
@@ -1,27 +1,12 @@
-$('.right').click(function() {
+$('.aboutMe').click(function() {
 	var currentSlide = $('.slide.active');
 	var nextSlide = currentSlide.next();
 
 	currentSlide.removeClass('active');
 	nextSlide.addClass('active');
-	console.log('why wont you work')
 
 	if (nextSlide.length == 0) {
 		$('.slide').first().addClass('active');
 	}
 })
-
-// $('.aboutMe').on("swiperight", function(event) {
-// 	var currentSlide = $('.slide.active');
-// 	var nextSlide = currentSlide.next();
-
-// 	currentSlide.removeClass('active');
-// 	nextSlide.addClass('active');
-// 	console.log('why wont you work')
-
-// 	if (nextSlide.length == 0) {
-// 		$('.slide').first().addClass('active');
-// 	}
-// })
-
 
