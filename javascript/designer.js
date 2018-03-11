32cecd85048d0af3838b5e0514edefc47c824bf8
Michael Ashley <nexues97@users.noreply.github.com>
2018-03-11 17:34:07 -0400

    Updated Pages with new styling and change Img to GIF
    Removed the Gallery page as it was redundant, set up styling for projects/contact page. Changed out img for a gif.

--- javascript/designer.js
+++ javascript/designer.js
@@ -1,6 +1,7 @@
 //Window resize checks
 
 var smallScreen = false;
+var medScreen = false;
 var largeScreen = false;
 
 $(document).ready(function() {
@@ -8,6 +9,11 @@ $(document).ready(function() {
         smallScreen = true;
     } else {
         smallScreen = false;
+    };
+    if($(window).width() >= 426 && $(window).width() <= 1025) {
+    	medScreen = true;
+    } else {
+    	medScreen = false;
     };
     if($(window).width() >= 1026) {
     	largeScreen = true;
@@ -22,6 +28,11 @@ $(window).resize(function() {
     } else {
         smallScreen = false;
     };
+    if($(window).width() >= 426 && $(window).width() <= 1025) {
+    	medScreen = true;
+    } else {
+    	medScreen = false;
+    };
     if($(window).width() >= 1026) {
     	largeScreen = true;
     } else {
@@ -31,12 +42,12 @@ $(window).resize(function() {
 
 // Menu button actions
 
-$(document).on('click','.icon',function(){
+$(document).on('click','.icon',function() {
 	$('.overlay').css('height', '100%');
 	$('.icon').css('display', 'none');
 })
 
-$(document).on('click','.close',function(){
+$(document).on('click','.close',function() {
 	
 	$('.overlay').css('height', '0%');
 	$('.icon').fadeIn(800).css('display', 'block');
@@ -45,7 +56,7 @@ $(document).on('click','.close',function(){
 
 // About me and badge carousel to click through when screen width <= 425
 
-$('.aboutMe').click(function() {
+$(document).on('click', '.rightArrow', function() {
 	if (smallScreen) {
 		var currentSlide = $('.slide.active');
 		var nextSlide = currentSlide.next();
@@ -59,7 +70,21 @@ $('.aboutMe').click(function() {
 	};
 });	
 
-// Hide menu button if screen width is >= 1026
+$(document).on('click', '.leftArrow', function() {
+	if (smallScreen) {
+		var currentSlide = $('.slide.active');
+		var nextSlide = currentSlide.prev();
+
+		currentSlide.removeClass('active');
+		nextSlide.addClass('active');
+
+		if (nextSlide.length == 0) {
+			$('.slide').last().addClass('active');
+		};
+	};
+});	
+
+// Hide menu button, and navigation arrows if screen width is >= 1026
 
 $(window).resize(function() {
 	if (largeScreen) {
